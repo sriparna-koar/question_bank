0b115cf4a979e021253678472441948974d6f8b2
Sriparna Koar <koarsk03@gmail.com>
2024-12-02 10:59:53 +0530

    first commit

--- src/app/createassessment/page.jsx
+++ src/app/createassessment/page.jsx
@@ -1,11 +1,28 @@
+// import AssessmentDetails from "@/components/AssessmentDetails";
+// import Layout from "@/components/Layout"; 
+
+// export default function LoginPage() {
+//   return (
+//     <Layout>
+//       <main className="flex items-center justify-center h-screen">
+//         <AssessmentDetails />
+//       </main>
+//     </Layout>
+//   );
+// }
+"use client";
+
+import { Suspense } from "react";
 import AssessmentDetails from "@/components/AssessmentDetails";
-import Layout from "@/components/Layout"; 
+import Layout from "@/components/Layout";
 
 export default function LoginPage() {
   return (
     <Layout>
       <main className="flex items-center justify-center h-screen">
-        <AssessmentDetails />
+        <Suspense fallback={<div>Loading assessment details...</div>}>
+          <AssessmentDetails />
+        </Suspense>
       </main>
     </Layout>
   );
